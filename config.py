924e2b6015ce1dda2e518944158dc8f9babbf982
LUD-aile <asharsenyan09@gmail.com>
2026-09-09 20:24:58 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "MY_BOT_TOKEN"
+ADMIN_ID = 1489336036  
+
+
+COORDINATORS = {
+    "Разработка": "@dev_coordinator_username",
+    "Маркетинг": "@marketing_coordinator_username",
+    "Дизайн": "@design_coordinator_username",
+    "Менеджмент": "@management_coordinator_username"
+}
+
